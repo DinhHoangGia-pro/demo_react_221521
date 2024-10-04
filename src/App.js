da3bce603435cf85985aab7361400489d8f5712b
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-04 13:41:30 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <header>Phần Header</header>
+      <body>Xin chào các bạn 221521</body>
+      <footer>Phần Footer</footer>
+    </html>
   );
 }
 
