fd8c43ba16adf1f8b5d86e3f2aa4c2f121e3fdfa
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-11 16:36:57 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,20 @@ const Home=()=>
         const tenlop="Xin chào 221521";
     
         const vieccanlam1=['Viec 1', 'Viec 2', 'Viec 3', 'Viec 4'];
+
+        const Themsinhvien=()=>
+            {
+                console.log("Test them sinh vien");
+                const sinhvienmoi={id:5,hoten:"Cherry Nguyen", lop:"221521" };
+                array2=[...array2, sinhvienmoi];
+            }
     
-        var array2=[{id:1,hoten:"Nguyễn Văn A", lop:"221406" },
-                            {id:2,hoten:"Nguyễn Văn Bình", lop:"221406" },
-                            {id:3,hoten:"Nguyễn Văn Cường", lop:"221406" },
-                            {id:4,hoten:"Trần Thị D", lop:"221406" }];
+        var array2=[{id:1,hoten:"Nguyễn Văn A", lop:"221521" },
+                            {id:2,hoten:"Nguyễn Văn Bình", lop:"221521" },
+                            {id:3,hoten:"Nguyễn Văn Cường", lop:"221521" },
+                            {id:4,hoten:"Trần Thị D", lop:"221521" }];
+            //Themsinhvien();
+        
     
         
         
@@ -38,6 +47,10 @@ const Home=()=>
                         return( 
                         <p>
                             <p>
+                            <span>ID:</span>
+                            <span>{phantu.id}</span>
+                            </p>
+                            <p>
                             <span>Họ tên:</span>
                             <span>{phantu.hoten}</span>
                             </p>
@@ -51,6 +64,10 @@ const Home=()=>
 
                     )
                  }
+
+                 <p style={{paddingTop:20}}>
+                    <input type="button" value="Thêm sinh viên" onClick={Themsinhvien.bind(this)} style={{backgroundColor:"ThreeDFace", padding:10}}/>
+                 </p>
     
             </body>
     
