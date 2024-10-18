0da9d8b11edd203da0836ee1849920291dd87e97
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-18 16:20:15 +0700

    Update ProductPage.js

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,24 +1,60 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const ProductPage=()=>
     {
+
+        const [listproduct, SetListProduct]=useState([]);/*Mới đầu cái mảng trong useState là rỗng*/
+        useEffect(()=>
+            {
+            
+                const LayDulieutuBackend= async ()=>
+                    {
+                        try
+                        {
+                            const res= await axios.get("https://fakestoreapi.com/products/");
+                            SetListProduct(res.data);
+            
+                        }catch (err)
+                        {
+                            console.log(err.message)
+                        }
+            
+                    }
+            
+                    LayDulieutuBackend();            
+
+            }
+        ,[]);
+    
         return (
             <div class="news_left">
                 
-                <div class="item">
-						<div class="div_img">
-							<a href="#">
+                {
+                    listproduct.map(phantu=>{
 
-									<img class="img-responsive" alt="" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"/>
-							</a>
-						</div>
-						<div class="div_product">
-								<a href="#" class="news-title">
-									Bia đen Đại Việt  
-							</a>
-							<span class="des_news">
-									Bia đen Đại Việt 120000.0000  6 lon x 1 thùng
-							</span>
-						</div>
-					</div>
+                        return (
+                            <div class="item">
+                            <div class="div_img">
+                                <a href="#">
+                                        <img class="img-responsive" alt="" src={phantu.image}/>
+                                </a>
+                            </div>
+                            <div class="div_product">
+                                    <a href="#" class="news-title">
+                                        {phantu.title}  
+                                </a>
+                                <span class="des_news">
+                                        {phantu.description}
+                                </span>
+                            </div>
+                        </div>
+                        );
+                    }
+
+                    )
+                }
+                
                 
             </div>
         );
