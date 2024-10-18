a32926b6299c8fc38e4d643c2824466885980dd8
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-18 16:02:53 +0700

    Add files via upload

---
+++ src/ProductPage.js
@@ -0,0 +1,26 @@
+const ProductPage=()=>
+    {
+        return (
+            <div class="news_left">
+                
+                <div class="item">
+						<div class="div_img">
+							<a href="#">
+
+									<img class="img-responsive" alt="" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"/>
+							</a>
+						</div>
+						<div class="div_product">
+								<a href="#" class="news-title">
+									Bia đen Đại Việt  
+							</a>
+							<span class="des_news">
+									Bia đen Đại Việt 120000.0000  6 lon x 1 thùng
+							</span>
+						</div>
+					</div>
+                
+            </div>
+        );
+    } 
+export default ProductPage;
